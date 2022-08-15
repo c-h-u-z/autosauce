8f1472c3b62380fdd69ed4f2de4d87b2dcc5b44a
EllisWitten <e.witten.2001@gmail.com>
2022-08-15 12:17:55 +0100

    add products page

---
+++ AutoSauceDemoTest/AutoSauceDemoTest/Lib/Pages/Sauce_ProductsPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSauceDemoTest.Lib.Pages
+{
+    public class Sauce_ProductsPage
+    {
+        #region Properties and fields
+        private IWebDriver _seleniumDriver;
+        private string _productsPageUrl = AppConfigReader.InventoryUrl;
+
+        #endregion
+        public Sauce_HomePage(IWebDriver seleniumDriver) => _seleniumDriver = seleniumDriver;
+    }
+}
